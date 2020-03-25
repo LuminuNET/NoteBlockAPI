9e1e2599d5c3c9b3487bae8bc6933d85244ad2db
Linus Groschke <linus@elektronisch.dev>
2020-03-25 21:25:56 +0100

    Improving the nbs-decoder
    - Using try-with-resources in decoder
    - adds "originalAuthor" field to song

--- src/main/java/com/xxmicloxx/NoteBlockAPI/utils/NBSDecoder.java
+++ src/main/java/com/xxmicloxx/NoteBlockAPI/utils/NBSDecoder.java
@@ -1,235 +1,134 @@
 package com.xxmicloxx.NoteBlockAPI.utils;
 
-import java.io.DataInputStream;
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-
-import com.xxmicloxx.NoteBlockAPI.model.CustomInstrument;
 import com.xxmicloxx.NoteBlockAPI.model.Layer;
 import com.xxmicloxx.NoteBlockAPI.model.Note;
 import com.xxmicloxx.NoteBlockAPI.model.Song;
+import org.bukkit.Bukkit;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Utils for reading Note Block Studio data
- *
  */
 public class NBSDecoder {
 
-	/**
-	 * Parses a Song from a Note Block Studio project file (.nbs)
-	 * @see Song
-	 * @param songFile .nbs file
-	 * @return Song object representing a Note Block Studio project
-	 */
-	public static Song parse(File songFile) {
-		try {
-			return parse(new FileInputStream(songFile), songFile);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return null;
+	private NBSDecoder() {
+
 	}
 
 	/**
-	 * Parses a Song from an InputStream
-	 * @see Song
-	 * @param inputStream of a Note Block Studio project file (.nbs)
-	 * @return Song object from the InputStream
+	 * Parses a song from a .nbs-file.
+	 *
+	 * @param songFile The .nbs-file
+	 * @return The song as {@link Song}
 	 */
-	public static Song parse(InputStream inputStream) {
-		return parse(inputStream, null); // Source is unknown -> no file
-	}
-
-	/**
-	 * Parses a Song from an InputStream and a Note Block Studio project file (.nbs)
-	 * @see Song
-	 * @param inputStream of a .nbs file
-	 * @param songFile representing a .nbs file
-	 * @return Song object representing the given .nbs file
-	 */
-	private static Song parse(InputStream inputStream, File songFile) {
-		HashMap<Integer, Layer> layerHashMap = new HashMap<Integer, Layer>();
-		byte biggestInstrumentIndex = -1;
-		try {
-			DataInputStream dataInputStream = new DataInputStream(inputStream);
-			short length = readShort(dataInputStream);
-			int firstcustominstrument = 10; //Backward compatibility - most of songs with old structure are from 1.12
-			int firstcustominstrumentdiff;
-			int nbsversion = 0;
+	public static Song parse(final File songFile) {
+		final Map<Integer, Layer> layerMap = new HashMap<>();
+		try (final DataInputStream dataInputStream = new DataInputStream(new FileInputStream(songFile))) {
+			short length = dataInputStream.readShort();
+			int firstCustomInstrumentIndex = 10;
+			int firstCustomInstrumentIndexDiff;
+			int version = 0;
 			if (length == 0) {
-				nbsversion = dataInputStream.readByte();
-				firstcustominstrument = dataInputStream.readByte();
-				if (nbsversion >= 3) {
-					length = readShort(dataInputStream);
+				version = dataInputStream.readByte();
+				firstCustomInstrumentIndex = dataInputStream.readByte();
+				if (version >= 3) {
+					length = dataInputStream.readShort();
 				}
 			}
-			firstcustominstrumentdiff = InstrumentUtils.getCustomInstrumentFirstIndex() - firstcustominstrument;
-			short songHeight = readShort(dataInputStream);
-			String title = readString(dataInputStream);
-			String author = readString(dataInputStream);
-			readString(dataInputStream); // original author
-			String description = readString(dataInputStream);
-			float speed = readShort(dataInputStream) / 100f;
-			dataInputStream.readBoolean(); // auto-save
-			dataInputStream.readByte(); // auto-save duration
-			dataInputStream.readByte(); // x/4ths, time signature
-			readInt(dataInputStream); // minutes spent on project
-			readInt(dataInputStream); // left clicks (why?)
-			readInt(dataInputStream); // right clicks (why?)
-			readInt(dataInputStream); // blocks added
-			readInt(dataInputStream); // blocks removed
-			readString(dataInputStream); // .mid/.schematic file name
-			if (nbsversion >= 4) {
-				dataInputStream.readByte(); // loop on/off
-				dataInputStream.readByte(); // max loop count
-				readShort(dataInputStream); // loop start tick
+			firstCustomInstrumentIndexDiff = InstrumentUtils.getCustomInstrumentFirstIndex() - firstCustomInstrumentIndex;
+			final short height = dataInputStream.readShort();
+			final String title = dataInputStream.readUTF();
+			final String author = dataInputStream.readUTF();
+			final String originalAuthor = dataInputStream.readUTF();
+			final String description = dataInputStream.readUTF();
+			final float speed = dataInputStream.readShort();
+			// The following information is unnecessary; we're skipping it
+			dataInputStream.readBoolean(); // Auto-save enabled
+			dataInputStream.readByte(); // Auto-save period
+			dataInputStream.readByte(); // Time signature
+			dataInputStream.readInt(); // Minutes spent for project
+			dataInputStream.readInt(); // Left clicks
+			dataInputStream.readInt(); // Right clicks
+			dataInputStream.readInt(); // Blocks added
+			dataInputStream.readInt(); // Blocks removed
+			dataInputStream.readUTF(); // .midi / .schem name
+
+			// Read additional information in version NBS 4 or higher
+			if (version >= 4) {
+				dataInputStream.readByte(); // Loop on/off
+				dataInputStream.readByte(); // Max Loops
+				dataInputStream.readShort(); // Loop start
 			}
-			short tick = -1;
+
+			short currentTick = -1;
 			while (true) {
-				short jumpTicks = readShort(dataInputStream); // jumps till next tick
-				//System.out.println("Jumps to next tick: " + jumpTicks);
-				if (jumpTicks == 0) {
-					break;
-				}
-				tick += jumpTicks;
-				//System.out.println("Tick: " + tick);
-				short layer = -1;
+				final short jumpTicks = dataInputStream.readShort();
+				if (jumpTicks == 0) break;
+
+				currentTick += jumpTicks;
+				short currentLayer = -1;
 				while (true) {
-					short jumpLayers = readShort(dataInputStream); // jumps till next layer
-					if (jumpLayers == 0) {
-						break;
-					}
-					layer += jumpLayers;
-					//System.out.println("Layer: " + layer);
-					byte instrument = dataInputStream.readByte();
+					final short jumpLayers = dataInputStream.readShort();
+					if (jumpLayers == 0) break;
 
-					if (firstcustominstrumentdiff > 0 && instrument >= firstcustominstrument){
-						instrument += firstcustominstrumentdiff;
+					currentLayer += jumpLayers;
+					byte instrumentIndex = dataInputStream.readByte();
+
+					if (firstCustomInstrumentIndexDiff > 0 && instrumentIndex >= firstCustomInstrumentIndex) {
+						instrumentIndex += firstCustomInstrumentIndexDiff;
 					}
 
-					byte key = dataInputStream.readByte();
+					final byte key = dataInputStream.readByte();
 					byte velocity = 100;
 					byte panning = 100;
 					short pitch = 0;
-					if (nbsversion >= 4) {
-						velocity = dataInputStream.readByte(); // note block velocity
-						panning = dataInputStream.readByte(); // note block panning
-						pitch = readShort(dataInputStream); // note block pitch
+
+					// Only read this information in version NBS 4 or higher
+					if (version >= 4) {
+						velocity = dataInputStream.readByte();
+						panning = dataInputStream.readByte();
+						pitch = dataInputStream.readShort();
 					}
 
-					setNote(layer, tick,
-							new Note(instrument /* instrument */, key/* note */, velocity, panning, pitch),
-							layerHashMap);
-				}
-			}
-
-			if (nbsversion > 0 && nbsversion < 3) {
-				length = tick;
-			}
-
-			for (int i = 0; i < songHeight; i++) {
-				Layer layer = layerHashMap.get(i);
-
-				String name = readString(dataInputStream);
-				if (nbsversion >= 4){
-					dataInputStream.readByte(); // layer lock
+					final Layer layer = layerMap.computeIfAbsent((int) currentLayer, index -> new Layer());
+					layer.setNote(currentTick, new Note(instrumentIndex, key, velocity, panning, pitch));
 				}
 
-				byte volume = dataInputStream.readByte();
-				if (nbsversion >= 2){
-					dataInputStream.readByte(); // layer stereo
+				if (version > 0 && version < 3) {
+					length = currentTick;
 				}
-				if (layer != null) {
-					layer.setName(name);
-					layer.setVolume(volume);
+
+				for (int i = 0; i < height; i++) {
+					final Layer layer = layerMap.get(i);
+					final String name = dataInputStream.readUTF();
+
+					// Only read this information in version NBS 4 or higher
+					if (version >= 4) {
+						dataInputStream.readByte(); // Whether layer is locked
+					}
+
+					final byte volume = dataInputStream.readByte();
+					if (version >= 2) {
+						dataInputStream.readByte();
+					}
+
+					if (layer != null) {
+						layer.setName(name);
+						layer.setVolume(volume);
+					}
 				}
-			}
-			//count of custom instruments
-			byte customAmnt = dataInputStream.readByte();
-			CustomInstrument[] customInstrumentsArray = new CustomInstrument[customAmnt];
 
-			for (int index = 0; index < customAmnt; index++) {
-				customInstrumentsArray[index] = new CustomInstrument((byte) index, 
-						readString(dataInputStream), readString(dataInputStream));
-				dataInputStream.readByte();//pitch
-				dataInputStream.readByte();//key
+				return new Song(speed, layerMap, height, length, title, author, description, songFile, firstCustomInstrumentIndex);
 			}
-
-			if (firstcustominstrumentdiff < 0){
-				ArrayList<CustomInstrument> customInstruments = CompatibilityUtils.getVersionCustomInstrumentsForSong(firstcustominstrument);
-				customInstruments.addAll(Arrays.asList(customInstrumentsArray));
-				customInstrumentsArray = customInstruments.toArray(customInstrumentsArray);
-			} else {
-				firstcustominstrument += firstcustominstrumentdiff;
-			}
-
-			return new Song(speed, layerHashMap, songHeight, length, title, 
-					author, description, songFile, firstcustominstrument, customInstrumentsArray);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (EOFException e) {
-			String file = "";
-			if (songFile != null) {
-				file = songFile.getName();
-			}
-			Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "Song is corrupted: " + file);
-		} catch (IOException e) {
+		} catch (final EOFException e) {
+			Bukkit.getLogger().severe("Song is corrupted: " + songFile.getName());
+		} catch (final IOException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-	/**
-	 * Sets a note at a tick in a song
-	 * @param layerIndex
-	 * @param ticks
-	 * @param note
-	 * @param layerHashMap
-	 */
-	private static void setNote(int layerIndex, int ticks, Note note, HashMap<Integer, Layer> layerHashMap) {
-		Layer layer = layerHashMap.get(layerIndex);
-		if (layer == null) {
-			layer = new Layer();
-			layerHashMap.put(layerIndex, layer);
-		}
-		layer.setNote(ticks, note);
-	}
-
-	private static short readShort(DataInputStream dataInputStream) throws IOException {
-		int byte1 = dataInputStream.readUnsignedByte();
-		int byte2 = dataInputStream.readUnsignedByte();
-		return (short) (byte1 + (byte2 << 8));
-	}
-
-	private static int readInt(DataInputStream dataInputStream) throws IOException {
-		int byte1 = dataInputStream.readUnsignedByte();
-		int byte2 = dataInputStream.readUnsignedByte();
-		int byte3 = dataInputStream.readUnsignedByte();
-		int byte4 = dataInputStream.readUnsignedByte();
-		return (byte1 + (byte2 << 8) + (byte3 << 16) + (byte4 << 24));
-	}
-
-	private static String readString(DataInputStream dataInputStream) throws IOException {
-		int length = readInt(dataInputStream);
-		StringBuilder builder = new StringBuilder(length);
-		for (; length > 0; --length) {
-			char c = (char) dataInputStream.readByte();
-			if (c == (char) 0x0D) {
-				c = ' ';
-			}
-			builder.append(c);
-		}
-		return builder.toString();
-	}
-	
 }
